a98075e3791fb401a6f7af253dd3f58a713e1858
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2022-01-31 23:20:43 +0700

    DataTable1

--- src/views/DataTable1.vue
+++ src/views/DataTable1.vue
@@ -3,7 +3,7 @@
     <b-container fluid>
       <b-row>
         <b-col>
-          <b-table :items="productItems"> </b-table>
+          <b-table :items="productItems" :fields="fields"> </b-table>
         </b-col>
       </b-row>
     </b-container>
@@ -13,17 +13,22 @@
 export default {
   data () {
     return {
+      fields: [
+        { key: 'productId', label: 'ไอดี' },
+        { key: 'name', label: 'ชื่อสินค้า' },
+        { key: 'price', label: 'ราคา' }
+      ],
       productItems: [
-        { productID: 1, name: 'Ipad gen1 64G Wifi', price: 11000.0 },
-        { productID: 2, name: 'Ipad gen2 64G Wifi', price: 12000.0 },
-        { productID: 3, name: 'Ipad gen3 64G Wifi', price: 13000.0 },
-        { productID: 4, name: 'Ipad gen4 64G Wifi', price: 14000.0 },
-        { productID: 5, name: 'Ipad gen5 64G Wifi', price: 15000.0 },
-        { productID: 6, name: 'Ipad gen6 64G Wifi', price: 16000.0 },
-        { productID: 7, name: 'Ipad gen7 64G Wifi', price: 17000.0 },
-        { productID: 8, name: 'Ipad gen8 64G Wifi', price: 18000.0 },
-        { productID: 9, name: 'Ipad gen9 64G Wifi', price: 19000.0 },
-        { productID: 10, name: 'Ipad gen10 64G Wifi', price: 20000.0 }
+        { productId: 1, name: 'Ipad gen1 64G Wifi', price: 11000.0 },
+        { productId: 2, name: 'Ipad gen2 64G Wifi', price: 12000.0 },
+        { productId: 3, name: 'Ipad gen3 64G Wifi', price: 13000.0 },
+        { productId: 4, name: 'Ipad gen4 64G Wifi', price: 14000.0 },
+        { productId: 5, name: 'Ipad gen5 64G Wifi', price: 15000.0 },
+        { productId: 6, name: 'Ipad gen6 64G Wifi', price: 16000.0 },
+        { productId: 7, name: 'Ipad gen7 64G Wifi', price: 17000.0 },
+        { productId: 8, name: 'Ipad gen8 64G Wifi', price: 18000.0 },
+        { productId: 9, name: 'Ipad gen9 64G Wifi', price: 19000.0 },
+        { productId: 10, name: 'Ipad gen10 64G Wifi', price: 20000.0 }
       ]
     }
   }
