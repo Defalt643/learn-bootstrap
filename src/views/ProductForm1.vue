b58de7f4119db19ac3493cf2083167e072fc554a
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2022-01-31 23:24:22 +0700

    ProductForm with modal

--- src/views/ProductForm1.vue
+++ src/views/ProductForm1.vue
@@ -1,50 +1,57 @@
 <template>
   <div>
-    <b-form @submit.prevent="submit" @reset.prevent="reset">
-      <b-form-group
-        id="form-group-product-name"
-        label="ชื่อสินค้า"
-        label-for="product-name"
-      >
-        <b-form-input
-          type="text"
-          id="product-name"
-          placeholder="ชื่อสินค้า Iphone XXX"
-          v-model="form.name"
-          :state="validateName"
-          autofocus
+    <b-button v-b-modal.modal-product variant="primary">เพิ่มสินค้า</b-button>
+    <b-modal
+      id="modal-product"
+      ref="modalProduct"
+      title="สินค้า"
+      @show="resetModal"
+      @hiddle="resetModal"
+      @ok="handleOk"
+    >
+      <b-form @submit.stop.prevent="submit" @reset.stop.prevent="reset">
+        <b-form-group
+          id="form-group-product-name"
+          label="ชื่อสินค้า"
+          label-for="product-name"
         >
-        </b-form-input>
-        <b-form-invalid-feedback :state="validateName">
-          ชื่อสินค้าต้องมีความยาวอย่างน้อย 3 ตัวอักษร
-        </b-form-invalid-feedback>
-      </b-form-group>
-      <b-form-group
-        id="form-group-product-price"
-        label="ราคาสินค้า"
-        label-for="product-price"
-      >
-        <b-form-input
-          type="number"
-          id="product-price"
-          placeholder="ราาสินค้า 10.0"
-          v-model="form.price"
-          :state="validatePrice"
+          <b-form-input
+            type="text"
+            id="product-name"
+            placeholder="ชื่อสินค้า Iphone XXX"
+            v-model="form.name"
+            :state="validateName"
+            autofocus
+          >
+          </b-form-input>
+          <b-form-invalid-feedback :state="validateName">
+            ชื่อสินค้าต้องมีความยาวอย่างน้อย 3 ตัวอักษร
+          </b-form-invalid-feedback>
+        </b-form-group>
+        <b-form-group
+          id="form-group-product-price"
+          label="ราคาสินค้า"
+          label-for="product-price"
         >
-        </b-form-input>
-        <b-form-invalid-feedback :state="validatePrice">
-          ราคาสินค้าจะต้องไม่น้อยกว่าหรือเท่ากับ 0 บาท
-        </b-form-invalid-feedback>
-        <b-button variant="primary" type="submit">Submit</b-button
-        ><b-button class="ml-1" type="reset" variant="danger">Reset</b-button>
-      </b-form-group>
-    </b-form>
-    <b-card>
-      <pre>
+          <b-form-input
+            type="number"
+            id="product-price"
+            placeholder="ราาสินค้า 10.0"
+            v-model="form.price"
+            :state="validatePrice"
+          >
+          </b-form-input>
+          <b-form-invalid-feedback :state="validatePrice">
+            ราคาสินค้าจะต้องไม่น้อยกว่าหรือเท่ากับ 0 บาท
+          </b-form-invalid-feedback>
+        </b-form-group>
+      </b-form>
+      <b-card>
+        <pre>
             {{ form }}
-        </pre
-      >
-    </b-card>
+        </pre>
+      </b-card>
+    </b-modal>
   </div>
 </template>
 <script>
@@ -63,17 +70,29 @@ export default {
       return this.form.name.length >= 3
     },
     validatePrice () {
-      return this.form.price >= 0 && this.form.price >= 0
+      return this.form.price !== '' && this.form.price >= 0
+    },
+    validateForm () {
+      return this.validateName && this.validatePrice
     }
   },
   methods: {
     submit () {
-      if (this.validateName && this.validatePrice) {
-        console.log(this.form)
-      }
+      console.log('Submit', this.form)
     },
     reset () {
       this.form = { id: 1, name: '', price: 0 }
+    },
+    resetModal (evt) {
+      this.reset()
+    },
+    handleOk (evt) {
+      evt.preventDefault()
+      if (!this.validateForm) return
+      this.submit()
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-product')
+      })
     }
   }
 }
